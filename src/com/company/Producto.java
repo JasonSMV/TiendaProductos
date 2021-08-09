8a882febdab1ba263584dbb5f15f82e5b368f59a
jasonsmv <jeisonsmv@gmail.com>
2021-08-08 20:12:12 -0500

    Se agrega el proyecto

---
+++ src/com/company/Producto.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Producto extends Tienda{
+    private int codigo;
+    private String nombre;
+    private String[] tipo;
+    private String expiracionFecha;
+    private int cantidad;
+    private int precioUnitario;
+
+}
---
+++ src/com/company/Tienda.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Tienda {
+
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private Producto[] productos;
+
+
+}
+
